afd67bd3fb444c14b6de2d9dd0a387d38b4620a5
Faisal Maarij <maarij_faisal@hotmail.com>
2022-06-24 09:57:32 +0200

    component,nav and onclik funtion

--- democomponent/src/components/header/navigation.js
+++ democomponent/src/components/header/navigation.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './navigation.css';
 
 class Navigation extends React.Component { 
-    navigation = () => {
-        console.log('Il y a eu un clique');
+    navigation = (destination) => { 
+        console.log('navigation vers '+destination);
     };
     render() {
         return(
             <nav>
-                <button onClick={this.navigation}>Accueil</button>
-                <button>Gallery</button>
-                <button>Contact</button>
+                
+                <button onClick={() => this.navigation('accueil')} >Accueil</button>
+                <button onClick={()=>this.navigation('gallery')} >gallery</button>
+
+                <button onClick={()=>this.navigation('contact')}>Contact</button>
+
+            
             </nav>
         );
     }
