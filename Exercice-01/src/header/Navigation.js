9b750a0cfba82e29ae4aac6caf282d8899ad26de
fmaarij <maarij_faisal@hotmail.com>
2022-06-24 21:48:22 +0200

    Les Composants - Les événements dans Exercice 1 ajouté

--- Exercice-01/src/header/Navigation.js
+++ Exercice-01/src/header/Navigation.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './navigation.css';
 class Navigation extends React.Component {
+    homePage =()=>{
+        console.log("accueil");
+    }
+    galleryPage =()=>{
+        console.log("gallery");
+    }
+    contactPage =()=>{
+        console.log("contact");
+    }
     render() {  
         return (
                 <ul>
-                    <li><button>HOME</button></li>
-                    <li><button>GALLERY</button></li>
-                    <li><button>CONTACT</button></li>
+                    <li><button onClick={this.homePage}>HOME</button></li>
+                    <li><button onClick={this.galleryPage}>GALLERY</button></li>
+                    <li><button onClick={this.contactPage}>CONTACT</button></li>
                 </ul>
         );
     }
