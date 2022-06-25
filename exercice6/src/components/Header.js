d3076f1c60a0909dd9d33e457f6f6b0c2ef14096
fmaarij <maarij_faisal@hotmail.com>
2022-06-25 08:49:09 +0200

    partie 3 done =eventListener added to the navebar

--- exercice6/src/components/Header.js
+++ exercice6/src/components/Header.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './header.css';
 class Header extends React.Component {
+    homePage=()=>{
+        console.log("Home page");
+    }
+    galleryPage=()=>{
+        console.log("gallery page");
+    }
+    contactPage=()=>{
+        console.log("contact page");
+    }
     render() {
         return(
             <nav>
                 <ul>
-                    <li><a>HOME</a></li>
-                    <li><a>GALLERY</a></li>
-                    <li><a>CONTACT</a></li>
+                    <li onClick={this.homePage}><a>HOME</a></li>
+                    <li onClick={this.galleryPage}><a>GALLERY</a></li>
+                    <li onClick={this.contactPage}><a>CONTACT</a></li>
                 </ul>
             </nav>
         );
